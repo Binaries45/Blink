ebf5b60d49fd61bfafedd4b0576e60401447c048
Bin_Laptop <ethan.ramdeen64@gmail.com>
2026-07-08 18:23:56 -0400

    nothing burger commit, thinking about syntax changes

--- src/main.zig
+++ src/main.zig
@@ -21,3 +21,32 @@ pub fn main(init: std.process.Init) !void {
     // std.debug.print("{f}\n", .{proc}
     try Compiler.compile(alloc, init.io, proc);
 }
+// TODO : Function decls as expressions
+// const main = () -> { ... }
+//
+// in order to keep things concise, we will have type inference operate on
+// functions to allow devs to omit complex types, or declare the types inline.
+// example:
+// const map = (collection: C, f: fn(T) T) C -> { ... }
+//
+// this map function declared more explicitly looks like
+// const map: fn(C, fn(T) T) C = (collection, f) -> { ... }
+//
+// however, this syntax can get bloated fast, so we allow users to omit the type decl
+// and just have param and return types be added onto the capture group with return types being void,
+// and empty capture types being an error.
+//
+// both should be allowed too, if someone decided to do that for whatever reason:
+// const map: fn(C, fn(T) T) C = (collection: C, f) C -> { ... }
+//
+// in this case type checking will ensure the capture types match the signature types
+//
+// this syntax will also extend naturally to support closures, lets say we call our map function on a list
+//
+// const new_list = list.map((a) -> { ... })
+//
+// although this does raise one issue on omitted return types, as to whether they should be inferred from the body,
+// or default to void. Inferrence does keep closures clean without breaking the "one right way" rule,
+// this is likely the best way to go as all type info will be inferred from the signature of map
+//
+// is he cooking? ^^^^^ 
